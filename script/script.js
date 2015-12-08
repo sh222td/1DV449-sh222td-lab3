f7eb50800f8cf1cebfe5b06c78eb92c5a2d26071
sh222td <sh222td@student.lnu.se>
2015-12-08 11:55:53 +0100

    Inforutan implementerad
    Hantering av inforutan också inplementerad, alltså man kan bara klicka
    på en plats i taget

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
+'use strict';
+
+// Global variables
 var map;
+var activeInfoWindow = '';
+
+// Initializes the map from the Google Map API
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 61.02, lng: 14.38},
         zoom: 6
     });
 }
-
 
 $.ajax({
     url: "./traffic.json",
@@ -21,9 +26,35 @@ $.ajax({
                 title: this.title
             });
             var list = document.getElementById('list');
-            var listObject = document.createElement('li')
+            var listObject = document.createElement('li');
             listObject.textContent = this.title;
             list.appendChild(listObject);
+
+            var infowindow = new google.maps.InfoWindow({
+                content: '<strong>'+this.title+'</strong><br>'+this.createddate+'<br>'+this.description+'<br>'+this.subcategory
+            });
+
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
+
+            // Function that handles the active infoWindows on the map
+            google.maps.event.addListener(marker, 'click', function()
+            {
+                if(activeInfoWindow != '')
+                {
+                    activeInfoWindow.close();
+                    activeInfoWindow = '';
+                }
+                var infoWindow = new google.maps.InfoWindow({content: '<strong>'+this.title+'</strong><br>'+this.createddate+'<br>'+this.description+'<br>'+this.subcategory});
+                infowindow.open(map, marker);
+                activeInfoWindow = infowindow;
+            });
+
         });
+
+
     }
 });
+
+
