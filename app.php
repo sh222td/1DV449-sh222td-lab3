3f3a59c8fc72e5ebc87cec26ab54bbf5b9aec2e2
sh222td <sh222td@student.lnu.se>
2015-12-11 16:59:25 +0100

    Omstrukturerat koden, kategorifiltrering implementerad

--- app.php
+++ app.php
@@ -14,6 +14,7 @@ class App {
         $view = "<!DOCTYPE html>
         <html>
             <head>
+                <meta charset = 'UTF-8'>
                 <link rel='stylesheet' href='style/style.css' media='screen'>
                 <script src='https://code.jquery.com/jquery-2.1.4.min.js'></script>
                 <script src='script/script.js'></script>
@@ -21,8 +22,14 @@ class App {
             <body>
                 <div id='listBox'>
                     <h2 id='headline'>Trafiklista</h2>
+                    <div id='buttonBox'>
+                    <input id='buttonAll' class='button' type='button' value='Samtliga'>
+                    <input id='buttonRoadTraffic' class='button' type='button' value='Vägtrafik'>
+                    <input id='buttonCollectiveTraffic' class='button' type='button' value='Kollektivtrafik'>
+                    <input id='buttonPlannedInterference' class='button' type='button' value='Planerad störning'>
+                    <input id='buttonAlternative' class='button' type='button' value='Övrigt'>
+                    </div>
                     <ul id='list'>
-
                     </ul>
                 </div>
                 <div id='map'></div>
--- script/script.js
+++ script/script.js
@@ -3,12 +3,13 @@
 // Global variables
 var map;
 var activeInfoWindow = '';
+var markers = [];
 
 // Initializes the map from the Google Map API
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 61.02, lng: 14.38},
-        zoom: 6
+        zoom: 5
     });
 }
 
@@ -17,44 +18,180 @@ $.ajax({
     dataType: "text",
     success: function(data) {
         var json = $.parseJSON(data);
+        var array = [];
 
-        $(json.messages).each(function() {
-            var myLatLng = {lat: this.latitude, lng: this.longitude};
-            var marker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                title: this.title
-            });
-            var list = document.getElementById('list');
-            var listObject = document.createElement('li');
-            listObject.textContent = this.title;
-            list.appendChild(listObject);
+        $("#buttonAll").click(function() {
+            clearList();
+            removeMarkers();
+            $(json.messages).each(function() {
+                var item = this;
+                var newDateFormat = new Date(parseInt(this.createddate.substr(6)));
+                console.log(this);
+                if (item.category === null) {
 
-            var infowindow = new google.maps.InfoWindow({
-                content: '<strong>'+this.title+'</strong><br>'+this.createddate+'<br>'+this.description+'<br>'+this.subcategory
-            });
+                    var newMarker = createMarker(item);
 
-            marker.addListener('click', function() {
-                infowindow.open(map, marker);
-            });
+                    var list = document.getElementById('list');
+                    var listObject = document.createElement('li');
+                    listObject.textContent = item.title;
+                    list.appendChild(listObject);
 
-            // Function that handles the active infoWindows on the map
-            google.maps.event.addListener(marker, 'click', function()
-            {
-                if(activeInfoWindow != '')
-                {
-                    activeInfoWindow.close();
-                    activeInfoWindow = '';
+                    toggleBounce(listObject, newMarker);
+                    infoWindow(item, newMarker, newDateFormat);
                 }
-                var infoWindow = new google.maps.InfoWindow({content: '<strong>'+this.title+'</strong><br>'+this.createddate+'<br>'+this.description+'<br>'+this.subcategory});
-                infowindow.open(map, marker);
-                activeInfoWindow = infowindow;
             });
-
         });
 
+        $("#buttonRoadTraffic").click(function() {
+            clearList();
+            removeMarkers();
+            $(json.messages).each(function() {
+                var item = this;
+                var newDateFormat = new Date(parseInt(this.createddate.substr(6)));
+                console.log(this);
+                if (item.category === 0) {
 
+                    var newMarker = createMarker(item);
+
+                    var list = document.getElementById('list');
+                    var listObject = document.createElement('li');
+                    listObject.textContent = item.title;
+                    list.appendChild(listObject);
+
+                    toggleBounce(listObject, newMarker);
+                    infoWindow(item, newMarker, newDateFormat);
+                }
+            });
+        });
+
+        $("#buttonCollectiveTraffic").click(function() {
+            clearList();
+            removeMarkers();
+            $(json.messages).each(function() {
+                var item = this;
+                var newDateFormat = new Date(parseInt(this.createddate.substr(6)));
+                if (item.category === 1) {
+                    var newMarker = createMarker(item);
+                    var list = document.getElementById('list');
+                    var listObject = document.createElement('li');
+                    listObject.textContent = item.title;
+                    list.appendChild(listObject);
+
+                    toggleBounce(listObject, newMarker);
+                    infoWindow(item, newMarker, newDateFormat);
+                }
+            });
+        });
+
+        $("#buttonPlannedInterference").click(function() {
+            clearList();
+            removeMarkers();
+            $(json.messages).each(function() {
+                var item = this;
+                var newDateFormat = new Date(parseInt(this.createddate.substr(6)));
+                console.log(this);
+                if (item.category === 2) {
+
+                    var newMarker = createMarker(item);
+
+                    var list = document.getElementById('list');
+                    var listObject = document.createElement('li');
+                    listObject.textContent = item.title;
+                    list.appendChild(listObject);
+
+                    toggleBounce(listObject, newMarker);
+                    infoWindow(item, newMarker, newDateFormat);
+                }
+            });
+        });
+
+        $("#buttonAlternative").click(function() {
+            clearList();
+            removeMarkers();
+            $(json.messages).each(function() {
+                var item = this;
+                var newDateFormat = new Date(parseInt(this.createddate.substr(6)));
+                console.log(this);
+                if (item.category === 3) {
+
+                    var newMarker = createMarker(item);
+
+                    var list = document.getElementById('list');
+                    var listObject = document.createElement('li');
+                    listObject.textContent = item.title;
+                    list.appendChild(listObject);
+
+                    toggleBounce(listObject, newMarker);
+                    infoWindow(item, newMarker, newDateFormat);
+                }
+            });
+        });
     }
 });
+
+function clearList() {
+    var list = document.getElementById('list');
+    list.textContent = "";
+}
+
+
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function removeMarkers() {
+    setMapOnAll(null);
+    markers = [];
+}
+
+function createMarker(item) {
+    var myLatLng = {lat: item.latitude, lng: item.longitude};
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: item.title
+    });
+    markers.push(marker);
+    return marker;
+
+}
+
+function infoWindow(item, marker, date) {
+
+    var infowindow = new google.maps.InfoWindow({
+        content: '<strong>'+item.title+'</strong><br>'+date.toLocaleString()+'<br>'+item.description+'<br>'+item.subcategory
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker, 'click', function()
+    {
+        if(activeInfoWindow != '')
+        {
+            activeInfoWindow.close();
+            activeInfoWindow = '';
+        }
+        var infoWindow = new google.maps.InfoWindow({content: '<strong>'+item.title+'</strong><br>'+date.toLocaleString()+'<br>'+item.description+'<br>'+item.subcategory});
+        infowindow.open(map, marker);
+        activeInfoWindow = infowindow;
+    });
+}
+
+// Function for managing the bounce effect when listobject is clicked
+function toggleBounce(listObject, marker) {
+    $(listObject).click(function() {
+        if (marker.getAnimation() != null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    });
+}
+
+
 
 
